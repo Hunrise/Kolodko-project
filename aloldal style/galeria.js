b4055a82f4e9a623931006ccd3d9a26a0b3c7117
Hunrise <nagy.hunor@diak.pataky.hu>
2025-03-16 21:07:56 +0100

    Add files via upload

---
+++ aloldal style/galeria.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const kolodkoCarousel = new bootstrap.Carousel("#kolodkoCarousel", {
+        interval: 3000,
+        wrap: true
+    });
+
+    const szobrokCarousel = new bootstrap.Carousel("#szobrokCarousel", {
+        interval: 3000,
+        wrap: true
+    });
+});
